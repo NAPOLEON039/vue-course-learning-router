3504b951249b4f225d922e2014e74382dc2c1e2a
NAPOLEON039 <nihar.raote@gmail.com>
2018-09-10 18:37:21 +0530

    Added parameters to route

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,6 +1,7 @@
 <template>
     <ul class="nav nav-pills">
         <router-link to="/" tag="li" class="nav-item"><a class="nav-link" :class="{active: this.$route.path == '/'}">Home</a></router-link>
-        <router-link to="/user" tag="li" class="nav-item"><a class="nav-link" :class="{active: this.$route.path == '/user'}">User</a></router-link>
+        <router-link to="/user/1" tag="li" class="nav-item"><a class="nav-link" :class="{active: this.$route.path == '/user/1'}">User 1</a></router-link>
+        <router-link to="/user/2" tag="li" class="nav-item"><a class="nav-link" :class="{active: this.$route.path == '/user/2'}">User 2</a></router-link>
     </ul>
 </template>
--- src/components/user/User.vue
+++ src/components/user/User.vue
@@ -1,11 +1,28 @@
 <template>
-    <h1>The User page</h1>
+    <div>
+        <h1>The User page</h1>
+        <hr>
+        <p>Loaded route: {{id}}</p>
+        <button class="btn btn-info" @click="goToHome()">Go to Home page</button>
+    </div>
 </template>
 
 <script>
 export default {
-    created() {
-        console.log(this.$route.path);
+    data() {
+        return {
+            id: this.$route.params.id
+        }
+    },
+    watch: {
+        $route(to, from) {
+            this.id = to.params.id;
+        }
+    },
+    methods: {
+        goToHome() {
+            this.$router.push('/');
+        }
     }
 }
 </script>
