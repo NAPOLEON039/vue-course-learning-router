3367a478a6f0dc551c73c7e6416a9f86a784915a
NAPOLEON039 <nihar.raote@gmail.com>
2018-09-17 18:22:26 +0530

    Learned adding dynamic component links and query parameters

--- src/components/user/UserDetail.vue
+++ src/components/user/UserDetail.vue
@@ -4,5 +4,10 @@
         <p>User loaded has ID: {{$route.params.id}}</p>
         <hr>
         <router-link to="/user" class="btn btn-primary">Back</router-link>
+        <router-link 
+            class="btn btn-primary" 
+            tag="button" 
+            :to="{name: 'userEdit', params: {id: $route.params.id}, query: {locale: 'en'}}"> Edit User
+        </router-link>
     </div>
 </template>
--- src/components/user/UserEdit.vue
+++ src/components/user/UserEdit.vue
@@ -1,3 +1,7 @@
 <template>
-    <h3>Edit user information</h3>
+    <div>
+        <h3>Edit user information</h3>
+        <hr>
+        <router-link :to="'/user/' + $route.params.id" class="btn btn-primary">Back</router-link>
+    </div>
 </template>
